84cc9d01f8fc1beac000da31165f70fc6d9b31f4
Henry <henryolson@icloud.com>
2023-06-27 15:26:05 -0500

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Footer from './components/Footer';
-import { HashRouter, Routes, Route, Outlet} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet} from 'react-router-dom';
 
 const background = {
   backgroundColor: '#EEA47F',
@@ -29,17 +29,17 @@ function App() {
   }
   return (
     <div style={background}>
-        <HashRouter>
+        <BrowserRouter>
           <Routes>
-            <Route path="https://hro0806.github.io/new-portfolio/" element={<Layout />}>
-              <Route path="https://hro0806.github.io/new-portfolio/" element={<About />}/>
-              <Route path="https://hro0806.github.io/new-portfolio/skills" element={<Skills />}/>
-              <Route path="https://hro0806.github.io/new-portfolio/portfolio" element={<Portfolio />}/>
-              <Route path="https://hro0806.github.io/new-portfolio/contact" element={<Contact />}/>
-              <Route path="https://hro0806.github.io/new-portfolio/resume" element={<Resume />}/>
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<About />}/>
+              <Route path="/skills" element={<Skills />}/>
+              <Route path="/portfolio" element={<Portfolio />}/>
+              <Route path="/contact" element={<Contact />}/>
+              <Route path="/resume" element={<Resume />}/>
             </Route>  
           </Routes>
-        </HashRouter>
+        </BrowserRouter>
     </div>
   );
 }
