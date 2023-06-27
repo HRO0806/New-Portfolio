5f0dfb47b0a99b0bcae8abdbfd4d5a7e815ea585
Henry <henryolson@icloud.com>
2023-06-27 14:42:27 -0500

    test

--- src/App.js
+++ src/App.js
@@ -31,12 +31,12 @@ function App() {
     <div style={background}>
         <HashRouter>
           <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route path="/" element={<About />}/>
-              <Route path="/skills" element={<Skills />}/>
-              <Route path="/portfolio" element={<Portfolio />}/>
-              <Route path="/contact" element={<Contact />}/>
-              <Route path="/resume" element={<Resume />}/>
+            <Route path="https://hro0806.github.io/new-portfolio/" element={<Layout />}>
+              <Route path="https://hro0806.github.io/new-portfolio/" element={<About />}/>
+              <Route path="https://hro0806.github.io/new-portfolio/skills" element={<Skills />}/>
+              <Route path="https://hro0806.github.io/new-portfolio/portfolio" element={<Portfolio />}/>
+              <Route path="https://hro0806.github.io/new-portfolio/contact" element={<Contact />}/>
+              <Route path="https://hro0806.github.io/new-portfolio/resume" element={<Resume />}/>
             </Route>  
           </Routes>
         </HashRouter>
